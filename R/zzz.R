7d4ebb006f894820582a094ef77f0a757a081090
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2017-03-27 14:13:10 +0800

    add helper fucntion .build.endpoint and .build.bucket.host

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,8 @@
   packageStartupMessage("Ross is a OSS API Wrapper for R.")
   op <- options()
   op.ross <- list(
-    ross.zone = "beijing"
+    ross.location = "beijing",
+    ross.internal = FALSE
   )
   toset <- !(names(op.ross) %in% names(op))
   if(any(toset)) options(op.ross[toset])
@@ -11,4 +12,36 @@
   AccessKeySecret=Sys.getenv("AccessKeySecret")
 
   invisible()
+}
+
+#' Build Endpoint host
+#'
+#' @param location bucket location
+#' @param internal whether access from aliyun ecs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.build.endpoint <- function(location="oss-cn-beijing", internal=TRUE){
+  if(internal){
+    domain <- "%s-internal.aliyuncs.com"
+  }else{
+    domain <- "%s.aliyuncs.com"
+  }
+  sprintf(domain, location)
+}
+
+#' Build Bucket host
+#'
+#' @param name bucket name
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.build.bucket.host <- function(name, ...){
+  endpoint <- .build.endpoint(...)
+  sprintf('http://%s.%s', name, endpoint)
 }
