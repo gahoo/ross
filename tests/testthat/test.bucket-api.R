b8f455f25be5ea18006e72621d9fee11bb0986ab
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2017-03-28 19:52:50 +0800

    add tests: PutBuckerWebsite, PutBuckerReferer, PubBucketLifcycle, GetBucket, GetBucketAcl; modify test: PutBucket reflow codes

--- tests/testthat/test.bucket-api.R
+++ tests/testthat/test.bucket-api.R
@@ -3,23 +3,18 @@ test_that("Bucket", {
   expect_equal(b$Name, 'igenecode')
 })
 
+###### PUT
 test_that("PutBucket", {
-  r <- PutBucket('igenecode')
+  r <- PutBucket('ross-test')
   expect_equal(r$status_code, 200)
-  expect_error(PutBucket('igenecode', acl = 'wrong-acl'))
-  expect_error(PutBucket('igenecode', StorageClass = 'wrong-class'))
+  expect_warning(r <- PutBucket('ross-test', StorageClass = 'IA'))
+  expect_equal(r$status_code, 409)
+  expect_error(PutBucket('ross-test', acl = 'wrong-acl'))
+  expect_error(PutBucket('ross-test', StorageClass = 'wrong-class'))
   expect_warning(PutBucket('test'))
 })
 
-test_that("DeleteBucket", {
-  r <- PutBucket('ross-test', StorageClass = 'IA')
-  expect_equal(r$status_code, 200)
-  r <- DeleteBucket('ross-test')
-  expect_equal(r$status_code, 204)
-})
-
 test_that("PutBucketLogging", {
-  r <- PutBucket('ross-test')
   r <- PutBucketLogging('ross-test', 'log-', 'ross-test')
   expect_equal(r$status_code, 200)
   r <- PutBucketLogging('ross-test', 'log-')
@@ -28,6 +23,47 @@ test_that("PutBucketLogging", {
   expect_equal(r$status_code, 200)
 })
 
-test_that("", {
+test_that("PutBucketWebsite", {
+  r <- PutBucketWebsite('ross-test', key='error.html', suffix='index.html')
+  expect_equal(r$status_code, 200)
+})
 
+test_that("PutBucketReferer", {
+  r <- PutBucketReferer('ross-test')
+  expect_equal(r$status_code, 200)
+  r <- PutBucketReferer('ross-test', AllowEmptyReferer=FALSE, RefererList='http://*.aliyun.com')
+  expect_equal(r$status_code, 200)
+  r <- PutBucketReferer('ross-test', AllowEmptyReferer=FALSE)
+  expect_equal(r$status_code, 200)
+})
+
+test_that("PutBucketLifecycle", {
+  r <- PutBucketLifecycle('ross-test', Prefix = 'upload_', Object.Days = 30)
+  expect_equal(r$status_code, 200)
+  r <- PutBucketLifecycle('ross-test', Prefix = 'upload_', Object.CreatedBeforeDate = Sys.Date()+7)
+  expect_equal(r$status_code, 200)
+  r <- PutBucketLifecycle('ross-test', Prefix = 'upload_', Multpart.Days = 5)
+  expect_equal(r$status_code, 200)
+  r <- PutBucketLifecycle('ross-test', Prefix = 'upload_', Object.Days = 30, Multpart.Days = 5)
+})
+
+###### Get
+test_that("GetBucket", {
+  r <- GetBucket('ross-test')
+  expect_equal(r$status_code, 200)
+  r <- GetBucket('ross-test', prefix='test/', marker = 'test/2.txt', max_keys = 5, delimiter = '/')
+  expect_equal(r$status_code, 200)
+})
+
+test_that("GetBucketAcl", {
+  r<-GetBucketAcl('ross-test')
+  expect_equal(r$status_code, 200)
+})
+
+###### Delete
+test_that("DeleteBucket", {
+  r <- PutBucket('ross-test')
+  expect_equal(r$status_code, 200)
+  r <- DeleteBucket('ross-test')
+  expect_equal(r$status_code, 204)
 })
