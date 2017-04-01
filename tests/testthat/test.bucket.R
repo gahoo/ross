e22ed2438eacb30cebad07d05732986a4c375e5e
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2017-04-01 19:18:59 +0800

    add tests for website, referer, lifecycle

--- tests/testthat/test.bucket.R
+++ tests/testthat/test.bucket.R
@@ -1,10 +1,9 @@
 test_that("Bucket", {
-  expect_error(b <- Bucket$new('ross-test'))
-  expect_error(b$rm())
   expect_message(b <- Bucket$new('ross-test', autoCreate=T))
   expect_silent(b$rm())
   # autoCreate=F
   expect_error(b <- Bucket$new('ross-test', autoCreate=F))
+  expect_warning(expect_error(b$rm()))
   # create
   expect_message(b$create(acl='public-read', StorageClass = 'IA'))
   expect_silent(b$refresh())
@@ -33,4 +32,59 @@ test_that("Bucket$logging", {
   expect_equal(b$logging, conf)
   expect_silent(b$logging <- NULL)
   expect_silent(b$rm())
+})
+
+test_that("Bucket$website", {
+  expect_message(b <- Bucket$new('ross-test', autoCreate=T))
+  expect_silent(b$website <- list(Suffix='index.html', Key='404.html'))
+  expect_equal(b$website, list(Suffix='index.html', Key='404.html'))
+  expect_message(b$website <- list(Suffix='Index.html'))
+  expect_equal(b$website, list(Suffix='Index.html', Key='404.html'))
+  expect_silent(b$website <- list(Suffix="", Key='error.html'))
+  expect_equal(b$website, list(Key='error.html'))
+  expect_silent(b$website <- list())
+  expect_silent(b$website <- NULL)
+  expect_equal(length(b$website), 0)
+  expect_silent(b$rm())
+})
+
+test_that("Bucket$referer", {
+  expect_message(b <- Bucket$new('ross-test', autoCreate=T))
+  expect_silent(b$referer <- list(AllowEmptyReferer=T, RefererList=c('*.igenecode.com', 'aliyun.com')))
+  b$referer <- list(AllowEmptyReferer=F)
+  expect_equal(b$referer$AllowEmptyReferer, 'false')
+  b$referer <- list(RefererList=c('*.igenecode.com', 'aliyun.com'))
+  expect_equal(b$referer$AllowEmptyReferer, 'true')
+  expect_equal(b$referer$RefererList, c(Referer='*.igenecode.com', Referer='aliyun.com'))
+  b$referer <- NULL
+  b$referer <- list()
+  expect_silent(b$rm())
+})
+
+test_that("BucketLifecycle", {
+  r <- PutBucket('ross-test')
+  expect_silent(life<-BucketLifecycle$new('ross-test', autoSave=T))
+  expect_equal(life$length, 0)
+  # add
+  expect_silent(life$add('upload_', Object.CreatedBeforeDate = "2017-04-01"))
+  expect_silent(life$add('upload_', Object.Days=5))
+  expect_silent(life$add('upload2_', Multpart.Days = 5))
+  expect_silent(life$add('Backup_', ID='backup-1', Object.Days=90, Multpart.Days = 5))
+  expect_error(life$add('upload2_'))
+  expect_equal(life$length, 3)
+  # remove
+  expect_silent(life$remove('upload_'))
+  expect_silent(life$remove(ID='backup-1'))
+  expect_equal(life$length, 1)
+  # clear
+  expect_silent(life$clear())
+  expect_equal(life$length, 0)
+  # autoSave=F
+  expect_silent(life<-BucketLifecycle$new('ross-test', F))
+  expect_silent(life$add('backup1_', ID='backup-1', Object.Days=90))
+  expect_silent(life$add('backup2_', ID='backup-2', Object.Days=90))
+  expect_silent(life$add('backup3_', ID='backup-3', Object.Days=90))
+  expect_silent(life$save())
+  expect_equal(life$length, 3)
+  r <- DeleteBucket('ross-test')
 })
