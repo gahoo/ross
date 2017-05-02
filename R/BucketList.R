cf8f243a53f619289a3f410b6bcaa6dfc9122943
Gahoolee <gahoolee@gmail.com>
2017-05-02 15:15:32 +0800

    better print and return values

--- R/BucketList.R
+++ R/BucketList.R
@@ -5,6 +5,7 @@
 #' @importFrom R6 R6Class
 #' @import xml2
 #' @import httr
+#' @import tibble
 #' @export
 #' @name BucketList
 #'
@@ -45,16 +46,33 @@ BucketList <- R6::R6Class("BucketList",
       response <- GetService(self$prefix, self$marker, self$max_keys)
       self$parseResponse(response)
     },
-    list = function(val) {
-      self$buckets <- val
+    list = function(){
+      tibble::tibble(
+        Name = private$getBucketSlot('Name'),
+        CreationDate = private$getBucketSlot('CreationDate'),
+        Location = private$getBucketSlot('Location'),
+        StorageClass = private$getBucketSlot('StorageClass'),
+        Owner = private$getBucketSlotOwner(),
+        Bucket = self$buckets)
+    },
+    print = function(){
+      print(self$list())
     }
   ),
   private = list(
     parseBuckets = function(doc) {
       buckets <- xpath2list(doc, '/ListAllMyBucketsResult/Buckets/Bucket')
-      lapply(buckets, function(x){
-        Bucket$new(x$Name, x$CreationDate, x$Location, x$StorageClass)
+      buckets <- lapply(buckets, function(x){
+        Bucket$new(x$Name, x$CreationDate, x$Location, x$StorageClass, autoCreate=F)
         })
+      names(buckets) <- sapply(buckets, function(x) x$Name)
+      buckets
+    },
+    getBucketSlot = function(name){
+      sapply(self$buckets, function(x) x[[name]])
+    },
+    getBucketSlotOwner = function(){
+      sapply(self$buckets, function(x) x[['Owner']]$DisplayName)
     }
   )
 )
