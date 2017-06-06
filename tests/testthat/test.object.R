09f7450118208ede7691959e53ec8232a7f0ca59
Gahoolee <gahoolee@gmail.com>
2017-06-06 14:56:59 +0800

    add tests for download, uplaod, acl, meta, exists add more tests for read;

--- tests/testthat/test.object.R
+++ tests/testthat/test.object.R
@@ -6,6 +6,11 @@ test_that("Object",{
   expect_equal(r$status_code, 200)
   # read
   expect_identical(o$read(), 'test')
+  expect_identical(o$read(2), 'te')
+  expect_identical(o$read(3), 'st')
+  expect_equal(length(o$read(2)), 0)
+  expect_silent(o$seek(1))
+  expect_equal(o$read(3), 'est')
   # url
   expect_silent(o$url())
   # append
@@ -26,16 +31,37 @@ test_that("Object",{
   expect_silent(o$saveRDS(1:10))
   # readRDS
   expect_identical(o$readRDS(), 1:10)
+  # download
+  tmp <- tempfile()
+  expect_silent(o$download(tmp, quiet=T))
+  # upload
+  expect_silent(o$upload(tmp))
   # copy
   o$copyTo('ross-test', 'test2.txt')
   o$copyFrom('ross-test', 'test2.txt')
+  # acl
+  expect_equal(o$acl, 'private')
+  expect_silent(o$acl <- 'public-read')
+  expect_equal(o$acl, 'public-read')
   # link
   expect_error(o$link)
   expect_silent(ol <- Object$new('ross-test', 'linked-test.txt'))
   expect_silent(ol$link <- '/ross-test/test.txt')
   expect_equal(ol$link, '/ross-test/test.txt')
+  # meta
+  expect_silent(o$meta <- list(a=1, b=2))
+  expect_identical(o$meta, list(a='1', b='2'))
+  expect_silent(o$meta <- list(c=3))
+  expect_identical(o$meta, list(a='1', b='2', c='3'))
+  expect_silent(o$meta <- list(c=NULL))
+  expect_identical(o$meta, list(a='1', b='2'))
+  expect_silent(o$meta <- NULL)
+  expect_length(o$meta, 0)
+  # exists
+  expect_true(o$exists())
   # delete
   expect_silent(o$delete())
+  expect_false(o$exists())
   deleteBucket('ross-test')
 })
 
