e5e496cb059623d9d9cc1e7ea4728deca6b7c061
Gahoolee <gahoolee@gmail.com>
2017-06-08 11:31:58 +0800

    remove .onLoad from helper.R

--- R/helper.R
+++ R/helper.R
@@ -1,19 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Ross is a OSS API Wrapper for R.")
-  op <- options()
-  op.ross <- list(
-    ross.location = "beijing",
-    ross.internal = FALSE,
-    ross.vpc = FALSE,
-    ross.debug = FALSE,
-    ross.aria2c = is.installed('aria2c')
-  )
-  toset <- !(names(op.ross) %in% names(op))
-  if(any(toset)) options(op.ross[toset])
-
-  invisible()
-}
-
 #' Build Endpoint host
 #'
 #' @param location bucket location
